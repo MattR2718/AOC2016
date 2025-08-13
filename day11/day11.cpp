5f265b146d78fc4746ebb7d22cccd13c62c1c773
Matthew Ranson <matthew.ranson2718@gmail.com>
2025-08-14 00:00:09 +0100

    much better day 11 and slow day 12

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <queue>
 #include <unordered_set>
+#include <unordered_map>
 
 #include <ctre.hpp>
 #include <boost/bimap.hpp>
@@ -39,7 +40,7 @@ int get_from_bimap(const boost::bimap<std::string, int>& name_to_id, const std::
     return -1;
 }
 
-std::string get_name_from_bimap(const boost::bimap<std::string, int>& name_to_id, int id) {
+std::string get_from_bimap(const boost::bimap<std::string, int>& name_to_id, int id) {
     auto it = name_to_id.right.find(id);
     if (it != name_to_id.right.end()) {
         return it->second;
@@ -55,162 +56,210 @@ bool in_bimap(const boost::bimap<std::string, int>& name_to_id, int id) {
     return name_to_id.right.find(id) != name_to_id.right.end();
 }
 
+
 struct State{
-    std::vector<Item> items;
-    int elevator_position;
-    const boost::bimap<std::string, int>* name_to_id;
+    int elevator;
+    std::vector<std::pair<int, int>> chip_floor_gen_floor;
 
-    bool operator==(const State& other) const {
-        return items == other.items && elevator_position == other.elevator_position;
-    }
-
-    void print() const {
-        std::cout<<"--------------------------------------------------------\n";
-        std::cout << "Elevator Position: " << elevator_position << "\n";
-        auto items__sorted = items;
-        std::ranges::sort(items__sorted, [](const Item& a, const Item& b) {
-            return a.floor > b.floor || (a.floor == b.floor && a.id < b.id);
-        });
-        for (const auto& item : items__sorted) {
-            std::cout << "Floor: " << item.floor << " " << item.get_name(*name_to_id) << (item.type == ItemType::Microchip ? " Microchip" : " Generator") << "\n";
+    void print(boost::bimap<std::string, int>& name_to_id) const {
+        std::cout << "Elevator is on floor: " << elevator << "\n";
+        int i = 0;
+        for (const auto& [chip_floor, gen_floor] : chip_floor_gen_floor) {
+            std::cout << get_from_bimap(name_to_id, i++) << ": \n";
+            std::cout << "\tChip is on floor: " << chip_floor << ", Generator is on floor: " << gen_floor << "\n";
         }
-        std::cout<<"--------------------------------------------------------\n";
-
     }
 
     bool is_valid() const {
-        // Check if the state is valid (no microchip is left with an incompatible generator)
-        for (const auto& item : items) {
-            if (item.type == ItemType::Microchip) {
-                bool has_compatible_generator = false;
-                int num_generators_on_floor = 0;
-                for(const auto& item2 : items){
-
-                    if(item2.type == ItemType::Generator && item2.id == item.id && item2.floor == item.floor){
-                        has_compatible_generator = true;
-                    }
-                    if(item2.type == ItemType::Generator && item2.floor == item.floor){
-                        num_generators_on_floor++;
-                    }
-                }
-                if(!has_compatible_generator && num_generators_on_floor > 0) {
-#if PRINT
-                    std::cout<<"Invalid state due to missing compatible generator for microchip "<<item.get_name(*name_to_id)<<"\n";
-                    print();
-#endif
-                    return false;
-                }
+        std::vector<int> gen_floors(4, 0);
+        for (const auto& [chip_floor, gen_floor] : chip_floor_gen_floor) {
+            gen_floors[gen_floor]++;
+        }
+        for (const auto& [chip_floor, gen_floor] : chip_floor_gen_floor) {
+            if (chip_floor != -1 && gen_floor != -1 && chip_floor != gen_floor && gen_floors[chip_floor] > 0) {
+                return false;
             }
         }
         return true;
     }
 
+    std::vector<State> get_next_states() {
+        std::vector<State> next_states;
 
-    std::vector<State> next_states(){
-        std::vector<State> states;
+        // Collect indexes of chips and generators on the current elevator floor
+        std::vector<int> chip_on_floor, gen_on_floor;
+        for (int i = 0; i < chip_floor_gen_floor.size(); i++) {
+            const auto& [chip_floor, gen_floor] = chip_floor_gen_floor[i];
+            if (chip_floor == elevator) chip_on_floor.push_back(i);
+            if (gen_floor == elevator) gen_on_floor.push_back(i);
+        }
 
-        for (int i = 0; i < items.size(); i++) {
-            if(items[i].floor != elevator_position) {
-                continue;
+        // Function to try moving a set of items by delta floors
+        auto try_move = [&](const std::vector<std::pair<int, bool>>& items, int delta) {
+            State new_state = *this;
+            new_state.elevator += delta;
+            for (auto [index, is_chip] : items) {
+                if (is_chip) new_state.chip_floor_gen_floor[index].first += delta;
+                else         new_state.chip_floor_gen_floor[index].second += delta;
             }
-            for (int direction : {-1, 1}) {
-                int new_floor = elevator_position + direction;
-                if (new_floor >= 0 && new_floor < 4) {
-                    State new_state{this->items, new_floor, this->name_to_id};
-                    new_state.items[i].floor = new_floor;
-                    if(new_state.is_valid()){
-                        states.push_back(new_state);
-                    }
+            if (new_state.is_valid()) next_states.push_back(new_state);
+        };
+
+        // Generate all combinations of 1 or 2 items to move
+        auto generate_moves = [&](const std::vector<int>& indexes, bool is_chip) {
+            // Single item moves
+            for (int idx : indexes) {
+                if (elevator < 3) try_move({{idx, is_chip}}, 1);
+                if (elevator > 0) try_move({{idx, is_chip}}, -1);
+            }
+            // Double item moves
+            for (size_t i = 0; i < indexes.size(); i++) {
+                for (size_t j = i + 1; j < indexes.size(); j++) {
+                    if (elevator < 3) try_move({{indexes[i], is_chip}, {indexes[j], is_chip}}, 1);
+                    if (elevator > 0) try_move({{indexes[i], is_chip}, {indexes[j], is_chip}}, -1);
                 }
+            }
+        };
+
+        // Move chips and generators
+        generate_moves(chip_on_floor, true);
+        generate_moves(gen_on_floor, false);
+
+        // Move chip-generator pairs that are together
+        for (int i = 0; i < chip_floor_gen_floor.size(); i++) {
+            const auto& [chip_floor, gen_floor] = chip_floor_gen_floor[i];
+            if (chip_floor == gen_floor && chip_floor == elevator) {
+                if (elevator < 3) try_move({{i, true}, {i, false}}, 1);
+                if (elevator > 0) try_move({{i, true}, {i, false}}, -1);
             }
         }
 
-        // Generate all pairs of items
-        for (int i = 0; i < items.size(); ++i) {
-            for (int j = i + 1; j < items.size(); ++j) {
-                if(items[i].floor == items[j].floor && items[i].floor == elevator_position){
-                    for (int direction : {-1, 1}) {
-                        int new_floor = elevator_position + direction;
-                        if (new_floor >= 0 && new_floor < 4) {
-                            State new_state{this->items, new_floor, this->name_to_id};
-                            new_state.items[i].floor = new_floor;
-                            new_state.items[j].floor = new_floor;
-                            if(new_state.is_valid()){
-                                states.push_back(new_state);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-#if PRINT
-        std::cout<<"Generated "<<states.size()<<" new states.\n";
-#endif
-        return states;
+        return next_states;
     }
 
-    bool is_goal(){
-        return std::all_of(items.begin(), items.end(), [](const Item& item) {
-            return item.floor == 3;
+
+    bool is_goal() const {
+        return std::all_of(chip_floor_gen_floor.begin(), chip_floor_gen_floor.end(), [](const auto& pair) {
+            return pair.first == 3 && pair.second == 3;
         });
     }
-
 };
 
+bool operator==(const State& lhs, const State& rhs) {
+    auto sorted_lhs = lhs.chip_floor_gen_floor;
+    auto sorted_rhs = rhs.chip_floor_gen_floor;
+    std::sort(sorted_lhs.begin(), sorted_lhs.end());
+    std::sort(sorted_rhs.begin(), sorted_rhs.end());
+    return lhs.elevator == rhs.elevator && sorted_lhs == sorted_rhs;
+}
 
 struct StateHash {
-    std::size_t operator()(const State& s) const noexcept {
-        std::size_t h = std::hash<int>{}(s.elevator_position);
-
-        for (const auto& item : s.items) {
-            // Hash each part of the Item
-            std::size_t item_hash = 0;
-            hash_combine(item_hash, std::hash<int>{}(item.floor));
-            hash_combine(item_hash, std::hash<int>{}(item.id));
-            hash_combine(item_hash, std::hash<int>{}(static_cast<int>(item.type)));
-
-            // Mix it into the State's hash
-            hash_combine(h, item_hash);
+    std::size_t operator()(const State& state) const {
+        std::size_t hash = std::hash<int>()(state.elevator);
+        auto sorted = state.chip_floor_gen_floor;
+        std::sort(sorted.begin(), sorted.end());
+        for (const auto& [chip, gen] : sorted) {
+            hash ^= std::hash<int>()(chip) + 0x9e3779b9 + (hash<<6) + (hash>>2);
+            hash ^= std::hash<int>()(gen)  + 0x9e3779b9 + (hash<<6) + (hash>>2);
         }
-        return h;
-    }
-
-private:
-    template <typename T>
-    static void hash_combine(std::size_t& seed, const T& value) {
-        seed ^= std::hash<T>{}(value) + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+        return hash;
     }
 };
 
 
 int bfs(State initial_state){
-    std::queue<std::pair<State, int>> queue;
+
     std::unordered_set<State, StateHash> visited;
+    std::queue<std::pair<State, int>> q;
+    q.push({initial_state, 0});
 
-    queue.push({initial_state, 0});
-    visited.insert(initial_state);
+    while (!q.empty()) {
+        auto [current_state, steps] = q.front();
+        q.pop();
 
-    while (!queue.empty()) {
-        State current = queue.front().first;
-        int steps = queue.front().second;
-        queue.pop();
-
-        if (current.is_goal()) {
+        if (current_state.is_goal()) {
             return steps;
-            break;
         }
 
-        for (const auto& next : current.next_states()) {
-            if (visited.find(next) == visited.end()) {
-                visited.insert(next);
-                queue.push({next, steps + 1});
+        if (visited.count(current_state) > 0) {
+            continue;
+        }
+        visited.insert(current_state);
+
+        for (const auto& next_state : current_state.get_next_states()) {
+            if (visited.count(next_state) == 0) {
+                q.push({next_state, steps + 1});
             }
         }
     }
+
     return -1;
 }
 
 
+
+int reconstruct_path(const std::unordered_map<State, State, StateHash>& start_parents,
+                    const std::unordered_map<State, State, StateHash>& end_parents,
+                    const State& meeting_point) {
+    std::vector<State> path;
+
+    // Reconstruct path from start to meeting point
+    for (State curr = meeting_point; start_parents.count(curr) > 0; curr = start_parents.at(curr)) {
+        path.push_back(curr);
+    }
+    std::reverse(path.begin(), path.end());
+
+    // Reconstruct path from end to meeting point
+    for (State curr = meeting_point; end_parents.count(curr) > 0; curr = end_parents.at(curr)) {
+        path.push_back(curr);
+    }
+
+    return path.size();
+}
+
+// Search for the shortest path using bidirectional BFS
+int bi_bfs(State initial_state){
+    State end_state{ 3, std::vector<std::pair<int, int>>(initial_state.chip_floor_gen_floor.size(), {3, 3}) };
+
+    std::unordered_set<State, StateHash> vs, ve;
+    std::queue<std::pair<State, int>> qs, qe;
+    qs.push({initial_state, 0});
+    qe.push({end_state, 1});
+    vs.insert(initial_state);
+    ve.insert(end_state);
+
+    std::unordered_map<State, int, StateHash> state_steps;
+    state_steps[initial_state] = 0;
+    state_steps[end_state] = 1;
+
+    while(!qs.empty() && !qe.empty()) {
+        if(qs.size() <= qe.size()){
+            auto [cur_state, steps] = qs.front();
+            qs.pop();
+
+            for (const auto& neighbour : cur_state.get_next_states()) {
+                if (vs.count(neighbour)) continue;
+                if(ve.count(neighbour)) return steps + state_steps[neighbour];
+                vs.insert(neighbour);
+                qs.push({neighbour, steps + 1});
+                state_steps[neighbour] = steps + 1;
+            }
+        } else {
+            auto [cur_state, steps] = qe.front();
+            qe.pop();
+
+            for (const auto& neighbour : cur_state.get_next_states()) {
+                if (ve.count(neighbour)) continue;
+                if(vs.count(neighbour)) return steps + state_steps[neighbour];
+                ve.insert(neighbour);
+                qe.push({neighbour, steps + 1});
+                state_steps[neighbour] = steps + 1;
+            }
+        }
+    }
+
+    return -1;
+}
 
 int main() {
     std::string linetxt;
@@ -221,7 +270,7 @@ int main() {
     boost::bimap<std::string, int> name_to_id;
     int id_counter = 0;
     int floor = 0;
-    State initial_state{ {}, 0 };
+    State initial_state{ 0, {} };
     while(std::getline(std::cin, linetxt)){
         
         if(auto match = ctre::search<"The [a-zA-Z]+ floor contains nothing relevant.">(linetxt)){
@@ -232,17 +281,17 @@ int main() {
                 if (auto chip = match.get<1>(); chip) {
                     name = chip.to_string();
                     if(in_bimap(name_to_id, name)){
-                        initial_state.items.push_back({ItemType::Microchip, get_from_bimap(name_to_id, name), floor});
+                        initial_state.chip_floor_gen_floor[get_from_bimap(name_to_id, name)].first = floor;
                     }else{
-                        initial_state.items.push_back({ItemType::Microchip, id_counter, floor});
+                        initial_state.chip_floor_gen_floor.emplace_back(std::make_pair(floor, -1));
                     }
                     
                 } else if (auto gen = match.get<2>(); gen) {
                     name = gen.to_string();
                     if(in_bimap(name_to_id, name)){
-                        initial_state.items.push_back({ItemType::Generator, get_from_bimap(name_to_id, name), floor});
+                        initial_state.chip_floor_gen_floor[get_from_bimap(name_to_id, name)].second = floor;
                     }else{
-                        initial_state.items.push_back({ItemType::Generator, id_counter, floor});
+                        initial_state.chip_floor_gen_floor.emplace_back(std::make_pair(-1, floor));
                     }
                 }
                 if(name_to_id.left.find(name) == name_to_id.left.end()) {
@@ -253,7 +302,7 @@ int main() {
         floor++;
     }
 
-    initial_state.name_to_id = &name_to_id;
+    //initial_state.name_to_id = &name_to_id;
 #if PRINT
     for (const auto& [name, id] : name_to_id.left) {
         std::cout << "Name: " << name << ", ID: " << id << "\n";
@@ -267,19 +316,16 @@ int main() {
     initial_state.print();
 #endif
 
-
-    p1 = bfs(initial_state);
+    p1 = bi_bfs(initial_state);
     std::cout<<"Part 1: "<< p1 << "\n";
 
 
     name_to_id.insert({"elerium", id_counter++});
     name_to_id.insert({"dilithium", id_counter++});
-    initial_state.elevator_position = 0;
-    initial_state.items.push_back({ItemType::Generator, get_from_bimap(name_to_id, "elerium"), 0});
-    initial_state.items.push_back({ItemType::Generator, get_from_bimap(name_to_id, "dilithium"), 0});
-    initial_state.items.push_back({ItemType::Microchip, get_from_bimap(name_to_id, "elerium"), 0});
-    initial_state.items.push_back({ItemType::Microchip, get_from_bimap(name_to_id, "dilithium"), 0});
+    initial_state.elevator = 0;
+    initial_state.chip_floor_gen_floor.push_back({0, 0});
+    initial_state.chip_floor_gen_floor.push_back({0, 0});
 
-    p2 = bfs(initial_state);
+    p2 = bi_bfs(initial_state);
     std::cout<<"Part 2: "<< p2 << "\n";
 }
---
+++ day12/day12.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include <ctre.hpp>// 9227731 high
+
+void run(const std::vector<std::string>& instructions, std::map<std::string, int>& registers){
+    for(int i = 0; i < instructions.size(); i++){
+        if(auto m = ctre::match<"cpy ([a-zA-Z]+) (\\w+)">(instructions[i])){
+            std::string from = m.get<1>().to_string();
+            std::string to = m.get<2>().to_string();
+            registers[to] = registers[from];
+        }else if(auto m = ctre::match<R"(cpy ((?:\+|-)?[0-9]+) (\w+))">(instructions[i])){
+            int from = m.get<1>().to_number<int>();
+            std::string to = m.get<2>().to_string();
+            registers[to] = from;
+        } else if(auto m = ctre::match<"inc (\\w+)">(instructions[i])){
+            registers[m.get<1>().to_string()]++;        
+        } else if(auto m = ctre::match<"dec (\\w+)">(instructions[i])){
+            registers[m.get<1>().to_string()]--;        
+        } else if (auto m = ctre::match<R"(jnz ((?:\+|-)?[0-9]+) ((?:\+|-)?[0-9]+))">(instructions[i])) {
+            int val = m.get<1>().to_number<int>();
+            if(val != 0) {
+                i += m.get<2>().to_number<int>() - 1;
+            }
+        } else if (auto m = ctre::match<R"(jnz ([a-zA-Z]+) ((?:\+|-)?[0-9]+))">(instructions[i])) {
+            if(registers[m.get<1>().to_string()] != 0) {
+                i += m.get<2>().to_number<int>() - 1;
+            }
+        }else{
+            std::cout<<"Unknown Instruction: "<<instructions[i]<<"\n";
+        }
+    }
+}
+
+int main() {
+    std::string linetxt;
+    std::vector<std::string> instructions;
+    while(std::getline(std::cin, linetxt)){
+        instructions.push_back(linetxt);
+    }
+
+    std::map<std::string, int> r1;
+    run(instructions, r1);
+    std::cout<<"Part 1: "<<r1["a"]<<"\n";
+
+    std::map<std::string, int> r2{{"c", 1}};
+    run(instructions, r2);
+    std::cout<<"Part 2: "<<r2["a"]<<"\n";
+
+}
