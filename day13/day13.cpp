dea29f5a271c346a5a345c4834f80543ecd617f1
Matthew Ranson <matthew.ranson2718@gmail.com>
2025-08-17 20:48:52 +0100

    faster day13

--- day13/day13.cpp
+++ day13/day13.cpp
@@ -8,12 +8,12 @@
 #include <algorithm>
 #include <numeric>
 #include <execution>
+#include <chrono>
 
 #include <ctre.hpp>
 
-bool wall(const std::pair<int, int> pos, int num){
-    auto [x, y] = pos;
-    uint32_t sum = x*x + 3*x + 2*x*y + y + y*y + num;
+inline bool wall(const uint64_t pos, int num){
+    uint32_t sum = (pos >> 32)*(pos >> 32) + 3*(pos >> 32) + 2*(pos >> 32)*(pos & 0xFFFFFFFF) + (pos & 0xFFFFFFFF) + (pos & 0xFFFFFFFF)*(pos & 0xFFFFFFFF) + num;
     return std::popcount(sum) % 2;
 }
 
@@ -30,23 +30,25 @@ int main() {
     std::string linetxt;
     std::getline(std::cin, linetxt);
     int f_num = std::stoi(linetxt);
-    std::pair<int, int> goal;
+    uint64_t goal = 0;
     if(f_num == 10){
-        goal = {7, 4};
+        goal = 7ull << 32 | 4;
     }else{
-        goal = {31, 39};
+        goal = 31ull << 32 | 39;
     }
-
-
 
     int p1 = 0, p2 = 0;
     bool p2_done = false;
-    std::unordered_set<std::pair<int, int>, PairHash> visited;
-    std::queue<std::pair<std::pair<int, int>, int>> q;
-    static const std::vector<std::pair<int, int>> dirs{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    std::array<bool, 10000> visited{0};
+    std::queue<std::pair<uint64_t, int>> q;
+    static const std::array<std::pair<int, int>, 4> dirs{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
 
-    q.push({{1, 1}, 0});
-    visited.insert({1, 1});
+    auto idx = [](uint64_t num){
+        return (num & 0xFFFFFFFF) * 100 + (num >> 32);
+    };
+
+    q.push({(1ull << 32 | 1), 0});
+    visited[idx((1ull << 32 | 1))] = true;
 
     while (!q.empty()) {
         auto [pos, steps] = q.front();
@@ -61,14 +63,14 @@ int main() {
         }
 
         for (const auto& [dx, dy] : dirs) {
-            std::pair<int, int> next = {pos.first + dx, pos.second + dy};
-            if (next.first >= 0 && next.second >= 0 && !visited.count(next) && !wall(next, f_num)) {
-                visited.insert(next);
+            if(!(pos >> 32) && dx == -1 || !(pos & 0xFFFFFFFF) && dy == -1) continue;
+            uint64_t next = (((pos >> 32) + dx) << 32 | (pos & 0xFFFFFFFF) + dy);
+            if (next >> 32 >= 0 && (next & 0xFFFFFFFF) >= 0 && !visited[idx(next)] && !wall(next, f_num)) {
+                visited[idx(next)] = true;
                 q.push({next, steps + 1});
             }
         }
     }
-
 
     std::cout << "Part 1: " << p1 << '\n';
     std::cout << "Part 2: " << p2 << '\n';
