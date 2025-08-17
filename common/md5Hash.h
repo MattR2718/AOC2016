e7f1ac267ab05d2f09c9de1081d89b2d68d9817e
Matthew Ranson <matthew.ranson2718@gmail.com>
2025-08-18 00:30:01 +0100

    day14 slow

---
+++ common/md5Hash.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <ostream>
+
+#ifdef HAVE_OPENSSL
+  #include <openssl/md5.h>
+#else
+  #include <cryptopp/hex.h>
+  #include <cryptopp/filters.h>
+  #include <cryptopp/md5.h>
+#endif // HAVE_OPENSSL
+
+namespace MD5Hash{
+
+    constexpr int MD5_HASH_LENGTH = 16;
+
+    struct Hash {
+        uint64_t _p1{0}, _p2{0};
+
+        Hash() = default;
+
+        char get_hex_char(int pos) const {
+            int c;
+            if (pos < 16) {
+                c = (_p1 >> ((15 - pos) * 4)) & 0xf;
+            } else {
+                c = (_p2 >> ((31 - pos) * 4)) & 0xf;
+            }
+            return (c < 10) ? ('0' + c) : ('a' + (c - 10));
+        }
+
+        uint8_t get_hex_value(int pos) const {
+            if (pos < 16) {
+                return (_p1 >> ((15 - pos) * 4)) & 0xf;
+            } else {
+                return (_p2 >> ((31 - pos) * 4)) & 0xf;
+            }
+        }
+
+        void set_hash(const uint8_t* hash_str) {
+            for (int i = 0; i < 8; i++) {
+                _p1 |= static_cast<uint64_t>(hash_str[i])     << (56 - i * 8);
+                _p2 |= static_cast<uint64_t>(hash_str[i + 8]) << (56 - i * 8);
+            }
+        }
+
+        Hash(const uint8_t* hash_str) {
+            set_hash(hash_str);
+        }
+
+        std::string to_string() const {
+            std::string s;
+            s.reserve(32);
+            for (int i = 0; i < 32; i++) {
+                s.push_back(get_hex_char(i));
+            }
+            return s;
+        }
+
+        friend std::ostream& operator<< (std::ostream& stream, const Hash& h){
+            stream << h.to_string();
+            return stream;
+        }
+        
+    };
+
+    #ifdef HAVE_OPENSSL
+    Hash md5(const std::string& input) {
+        unsigned char digest[MD5_DIGEST_LENGTH]; // MD5_DIGEST_LENGTH = 16
+        MD5(reinterpret_cast<const unsigned char*>(input.data()), input.size(), digest);
+
+        return Hash(digest);
+    }
+
+    #else
+
+    Hash md5(const std::string& input) {
+        CryptoPP::MD5 hash;
+        CryptoPP::byte digest[CryptoPP::MD5::DIGESTSIZE]; // 16 bytes
+
+        // Calculate MD5 hash
+        hash.CalculateDigest(digest, reinterpret_cast<const CryptoPP::byte*>(input.data()), input.size());
+
+        // Convert the first 4 bytes of digest into a uint32_t (big-endian like your OpenSSL code)
+        return Hash(digest);
+    }
+
+    #endif // HAVE_OPENSSL
+};
--- day14/day14.cpp
+++ day14/day14.cpp
@@ -1,55 +1,105 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <climits>
+#include <map>
 
 #include <ctre.hpp>
 
+#include <md5Hash.h>
 #include <timer.h>
+
+std::map<std::string, MD5Hash::Hash> cache;
+std::map<std::string, MD5Hash::Hash> long_cache;
+
+uint8_t three(const MD5Hash::Hash& h){
+    for(int i = 0; i < 32 - 2; i++){
+        if(h.get_hex_value(i) == h.get_hex_value(i + 1) 
+        && h.get_hex_value(i + 1) == h.get_hex_value(i + 2))
+        {
+            return h.get_hex_value(i);
+        }
+    }
+    return UINT8_MAX;
+}
+
+bool five(const MD5Hash::Hash& h, uint8_t v){
+    for(int i = 0; i < 32 - 4; i++){
+        bool found = true;
+        for(int j = i; j < i + 5; j++){
+            if(h.get_hex_value(j) != v){
+                found = false;
+                break;
+            }
+        }
+        if(found){
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename f>
+int solve(const std::string& salt, f func){
+    auto make_hash_input = [&](int index) {
+        char numbuf[32];
+        auto len = sprintf(numbuf, "%d", index);
+        return salt + std::string(numbuf, len);
+    };
+
+    int num_found = 0;
+    int i = 0;
+    int p1 = 0;
+    while(num_found < 64){
+        std::string in = make_hash_input(i);
+        uint8_t val = three(func(in));
+        if(val < UINT8_MAX){
+            for(int j = 1; j < 1001; j++){
+                std::string in2 = make_hash_input(i + j);
+                if(five(func(in2), val)){
+                    num_found++;
+                    p1 = i;
+                    break;
+                }
+            }
+        }
+        i++;
+    }
+
+    return p1;
+}
 
 int main() {
     Timer::ScopedTimer t("Day 14");
+    std::string salt;
+    std::getline(std::cin, salt);
 
-    std::string linetxt;
-    int dir{0}, x{0}, y{0};
-    std::set<std::pair<int, int>> visited{{0, 0}};
-    bool p2_done{false};
-    while(std::getline(std::cin, linetxt)){
-        // Get directions in the from ([LR])(\\d+) using ctre
-        for (auto match: ctre::search_all<"([LR])(\\d+)">(linetxt)){
-            if (match.get<1>() == "L") {
-                dir = (dir + 3) % 4; // Turn left
-            } else if (match.get<1>() == "R") {
-                dir = (dir + 1) % 4; // Turn right
-            }
-
-            int steps = match.get<2>().to_number<int>();
-
-            if(!p2_done){
-                for(int i = 0; i < steps; i++){
-                    switch(dir){
-                        case 0: y--; break; // North
-                        case 1: x++; break; // East
-                        case 2: y++; break; // South
-                        case 3: x--; break; // West
-                    }
-                    if(visited.count({x, y}) == 0) {
-                        visited.insert({x, y});
-                    }else{
-                        p2_done = true;
-                        std::cout << "Part 2: " << (std::abs(x) + std::abs(y)) << "\n";
-                    }
-                }
-            }else{
-                switch(dir){
-                    case 0: y -= steps; break; // North
-                    case 1: x += steps; break; // East
-                    case 2: y += steps; break; // South
-                    case 3: x -= steps; break; // West
-                }
-            }
-            
+    int p1 = solve(salt, [](std::string str){
+        if(cache.contains(str)){
+            return cache.at(str);
+        }else{
+            auto h = MD5Hash::md5(str);
+            cache[str] = h;
+            return h;
         }
+    });
 
-        std::cout << "Part 1: " << (std::abs(x) + std::abs(y)) << "\n";
-    }
+    std::cout<<"Part 1: " << p1 <<'\n';
+
+
+    int p2 = solve(salt, [](std::string str){
+        if(long_cache.contains(str)){
+            return long_cache.at(str);
+        }else{
+            MD5Hash::Hash h = MD5Hash::md5(str);
+            for(int i = 0; i < 2016; i++){
+                h = MD5Hash::md5(h.to_string());
+            }
+            long_cache[str] = h;
+            return h;
+        }
+    });
+
+    std::cout<<"Part 2: " << p2 <<'\n';
+    
 }
